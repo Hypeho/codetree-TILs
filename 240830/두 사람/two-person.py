cba1d6650fd10941940a304967ef39286ac0cde2
hypo <128954674+Hypeho@users.noreply.github.com>
2024-08-30 13:30:36 +0900

    Title: 두 사람, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240830/두 사람/two-person.py
@@ -0,0 +1,10 @@
+a = input().split()
+age1, gen1 = int(a[0]), str(a[1])
+
+b = input().split()
+age2, gen2 = int(b[0]), str(b[1])
+
+if (age1>=19 or age2>=19) and (gen1=='M' or gen2=='M'):
+    print(1)
+else:
+    print(0)
