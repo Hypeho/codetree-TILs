f0b1c9f1723f5fd0b29ac14d3254ea5149a2d839
hypo <128954674+Hypeho@users.noreply.github.com>
2024-08-30 13:16:35 +0900

    Title: 장학금, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240830/장학금/scholarship.py
+++ 240830/장학금/scholarship.py
@@ -1,9 +1,8 @@
 a,b = map(int, input().split())
 
-if a>=90:
-    if b>=95:
-        print(100000)
-    elif b>=90:
-        print(50000)
+if a>=90 and b>=95:
+    print(100000)
+elif a>=0 and b>=90:
+    print(50000)
 else:
     print(0)
